251b3ad8ea412098c4ba5307a3520a67de7dd493
Injoon Hwang <injoon0617@gmail.com>
2021-02-18 00:23:40 +0900

    Show profiles on new window

--- scripts/chats.js
+++ scripts/chats.js
@@ -1,13 +1,13 @@
 const friendsTiles = document.getElementsByClassName("friends-tile");
-console.log(Array.from(friendsTiles));
 
 Array.from(friendsTiles).forEach((element) => {
     element.addEventListener("click", (evt) => {
         const name = evt.target.getElementsByClassName("friends-tile__name")[0]
             .innerText;
         window.open(
-            `./chats/chat-${name.toLowerCase()}.html`,
-            `Chat | ${name}`
+            `../chats/chat-${name.toLowerCase()}.html`,
+            `Chat | ${name}`,
+            "width=450, height=680"
         );
     });
 });
---
+++ scripts/profile.js
@@ -0,0 +1,15 @@
+const profileImgs = document.getElementsByClassName("friends-tile__img");
+
+Array.from(profileImgs).forEach((element) => {
+    element.addEventListener("click", (evt) => {
+        evt.stopPropagation();
+        const name = evt.target.parentElement.getElementsByClassName(
+            "friends-tile__name"
+        )[0].innerText;
+        window.open(
+            `../profiles/profile-${name.toLowerCase()}.html`,
+            `Profile | ${name}`,
+            "width=450, height=680"
+        );
+    });
+});
